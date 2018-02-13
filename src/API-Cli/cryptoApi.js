d45b9a1744f0f6682b5edce1e6f9b03a050bbb4a
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-13 16:12:00 +0100

    :zap: fix API-Cli

--- src/API-Cli/cryptoApi.js
+++ src/API-Cli/cryptoApi.js
@@ -132,49 +132,78 @@ function showResult(data) {
 /*PROBANDO A METER COIN EN EL CALL*/
 
 
-let cryptoApi; let CryptoHistorical;
-(function () {
-    
-    
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const baseUrl = "https://api.coinmarketcap.com/v1/ticker"
-    const preHistoricalPriceURL = "https://min-api.cryptocompare.com/data/histominute?fsym=" 
-    const postHistoricalPriceURL = "&tsym=EUR&limit=20&aggregate=3&e=CCCAGG"
+//let cryptoApi;
+//let CryptoHistorical;
+// (function () {
 
-    cryptoApi = {
-        baseUrl,
-        call: function () {
-            return fetch(proxyurl + baseUrl)
+
+//     const proxyurl = "https://cors-anywhere.herokuapp.com/";
+//     const baseUrl = "https://api.coinmarketcap.com/v1/ticker"
+//     const preHistoricalPriceURL = "https://min-api.cryptocompare.com/data/histominute?fsym="
+//     const postHistoricalPriceURL = "&tsym=EUR&limit=20&aggregate=3&e=CCCAGG"
+
+//     cryptoApi = {
+//         baseUrl,
+//         call: function () {
+//             return fetch(proxyurl + baseUrl)
+//                 .then(res => res.json())
+//         },
+//         getCoins: function () {
+//             return this.call().then(res => res)
+//         }
+//     }
+
+//     CryptoHistorical = {
+//         preHistoricalPriceURL,
+
+//         coin: "BTC",
+//         call: function () {
+//             return fetch(proxyurl + preHistoricalPriceURL + this.coin + postHistoricalPriceURL)
+//                 .then(res => res.json())
+//             this.call().then(res => res)
+//         },
+//     }
+
+// })()
+//export default cryptoApi;
+
+//===========================
+
+class CryptoApi {
+     proxyurl="https://cors-anywhere.herokuapp.com/"
+
+    call = (path) =>  {
+        return  fetch(path)
                 .then(res => res.json())
-        },
-        getCoins: function () {
-            return this.call().then(res => res)
-        }
     }
-
-    CryptoHistorical = {
-        preHistoricalPriceURL,
-
-        coin:"BTC",
-        call: function () {
-            return fetch(proxyurl + preHistoricalPriceURL+this.coin+postHistoricalPriceURL)
-                .then(res => res.json())
-                this.call().then(res => res)
-        },
+    getCoins = ()=>  {
+        let url = "https://api.coinmarketcap.com/v1/ticker"
+        return this.call(this.proxyurl + url)
     }
+    getConiHistorical = (coin = false) => {
+        if(!coin) throw new Error ('getHistorical(...) <-- need coin to search...')
+        let url = `https://min-api.cryptocompare.com/data/histominute?fsym=${coin}&tsym=EUR&limit=20&aggregate=3&e=CCCAGG`
+        return this.call(this.proxyurl + url)
+    }
+}
+const cryptoApi = new CryptoApi()
 
-})()
-export default cryptoApi;
+export default  cryptoApi
+
+
+
+
+
+
+
 
 //Here you can teste the API and see the results in browser terminal
 
 //cryptoApi.getCoins().then(res => showResult(res))
 //CryptoHistorical.call().then(res => showResult(res))
+cryptoApi.getCoins().then(res => showResult(res))
 
-// function showResult(data) {
-//     console.log(data)
-// }
-
-
-
+function showResult(data) {
+    console.log(data)
+}
 
