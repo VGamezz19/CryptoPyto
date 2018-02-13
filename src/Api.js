e2736ba8e6c34e71b6d1d376090184468a0b6ac2
phortela1n <pablohortelano2@gmail.com>
2018-02-13 12:10:28 +0100

    Implementig new api methods

--- src/Api.js
+++ src/Api.js
@@ -49,6 +49,7 @@ function showResult(data){
 */
 
 //LLAMADAS DE LAS DOS APIS COMBINADAS
+/*FUNCIONA
 
 let cryptoApi; let CryptoHistorical;
 (function () {
@@ -86,7 +87,88 @@ function showResult(data) {
     console.log(data)
 }
 
+*/
+
+/*SIMPLIFICANDO Y ELIMINANDO REPETICIONES
+
+let cryptoApi; let CryptoHistorical;
+(function () {
+    
+    let coin = "BTC";
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const baseUrl = "https://api.coinmarketcap.com/v1/ticker"
+    const preHistoricalPriceURL = "https://min-api.cryptocompare.com/data/histominute?fsym=" + coin + "&tsym=EUR&limit=20&aggregate=3&e=CCCAGG"
+
+    cryptoApi = {
+        baseUrl,
+        call: function () {
+            return fetch(proxyurl + baseUrl)
+                .then(res => res.json())
+        },
+        getCoins: function () {
+            return this.call().then(res => res)
+        }
+    }
+
+    CryptoHistorical = {
+        preHistoricalPriceURL,
+        call: function () {
+            return fetch(proxyurl + preHistoricalPriceURL)
+                .then(res => res.json())
+                this.call().then(res => res)
+        },
+    }
+
+})()
+
+cryptoApi.getCoins().then(res => showResult(res))
+CryptoHistorical.call().then(res => showResult(res))
+
+function showResult(data) {
+    console.log(data)
+}
+
+*/
+
+/*PROBANDO A METER COIN EN EL CALL*/
 
 
+let cryptoApi; let CryptoHistorical;
+(function () {
+    
+    
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const baseUrl = "https://api.coinmarketcap.com/v1/ticker"
+    const preHistoricalPriceURL = "https://min-api.cryptocompare.com/data/histominute?fsym=" + coin  
+    const postHistoricalPriceURL = "&tsym=EUR&limit=20&aggregate=3&e=CCCAGG"
 
+    cryptoApi = {
+        baseUrl,
+        call: function () {
+            return fetch(proxyurl + baseUrl)
+                .then(res => res.json())
+        },
+        getCoins: function () {
+            return this.call().then(res => res)
+        }
+    }
 
+    CryptoHistorical = {
+        preHistoricalPriceURL,
+
+        coin:"USD",
+        call: function () {
+            return fetch(proxyurl + preHistoricalPriceURL+this.coin+postHistoricalPriceURL)
+                .then(res => res.json())
+                this.call().then(res => res)
+        },
+    }
+
+})()
+
+cryptoApi.getCoins().then(res => showResult(res))
+CryptoHistorical.call().then(res => showResult(res))
+
+function showResult(data) {
+    console.log(data)
+}
