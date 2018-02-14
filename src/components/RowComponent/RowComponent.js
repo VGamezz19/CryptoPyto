00a43065efdccb5d2fa99cde77a07d080d18b803
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-14 12:37:10 +0100

    :art: loaderNieto style

--- src/components/RowComponent/RowComponent.js
+++ src/components/RowComponent/RowComponent.js
@@ -3,14 +3,18 @@ import ColChart from './components/ColChart'
 import ColName from './components/ColName'
 import ColPrice from './components/ColPrice'
 import ColPercent from './components/ColPercent'
+import LoaderNiesto from './components/LoaderNietos'
 class RowComponent extends Component{
+    
     render(){
+        console.log(this.props)
         return(
             <tr className='rowTr'>
                 <ColName/>
-                <ColPrice/>
+                <LoaderNiesto/>
+                {/* <ColPrice/>
                 <ColChart/>
-                <ColPercent/>
+                <ColPercent/> */}
             </tr>
         )
     }
---
+++ src/components/RowComponent/components/LoaderNietos.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default function LoaderNiesto() {
+    return (<td class="progress">
+        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+    </td>
+
+    )
+}
