52990076591955b231505441b5b615281af87c3c
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-15 18:07:42 +0100

    :art: fix color grafic

--- src/components/ContentApp/RowComponent/components/ColChartRealTime.js
+++ src/components/ContentApp/RowComponent/components/ColChartRealTime.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import C3Chart from 'react-c3js';
+import C3Chart, { d2 } from 'react-c3js';
+
 import 'c3/c3.css';
 function ColChartRealTime(props) {
 
   const data = {
     columns: [
-      ['data2', 50, 20, 10, 40, 15, 25]
+      ['coin', ...props.chartValueRealTime]
+    ],
+    colors: {
+      coin: '#ff0000'
+    }
 
-    ]
-    
   };
   return (
-    <td><C3Chart data={data} size={{ height: 200 }} /></td>
+    <td><C3Chart
+      data={data}
+      size={{ height: 200 }}
+      axis={{
+        y: {
+          tick: {
+            format: (e) => e.toFixed(2)
+          }
+        },
+      }} /></td>
   )
-
 }
 export default ColChartRealTime
