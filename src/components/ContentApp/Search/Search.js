1a992ec3a8424f5710f732fa1ac4282db394d93f
VGamezz19 <victor.gamez1596@gmail.com>
2018-02-16 11:29:50 +0100

    :pencil2: refactor Search

--- src/components/ContentApp/Search/Search.js
+++ src/components/ContentApp/Search/Search.js
@@ -1,9 +1,11 @@
 
 import React, { Component } from 'react';
-import SearchInput, { createFilter } from 'react-search-input'
-import { CSSTransitionGroup } from 'react-transition-group'
-import RowComponent from '../RowComponent/RowComponent'
-const KEYS_TO_FILTERS = ['name', 'symbol', 'id' ]
+import { createFilter } from 'react-search-input'
+import InputSearch from './components/InputSearch'
+import SelectInput from './components/SelectInput'
+import ContentRowSelect from './components/ContentRowSelect'
+
+const KEYS_TO_FILTERS = ['name', 'symbol', 'id']
 
 export default class Search extends Component {
   constructor() {
@@ -28,89 +30,18 @@ export default class Search extends Component {
   onClickCoin = (coin) => {
     this.setState(prevState => ({
       inputValue: '',
-      coinSelect:  [coin].concat(prevState.coinSelect)
+      coinSelect: [coin].concat(prevState.coinSelect)
     }))
   }
 
   render() {
     const filterCoins = this.state.cryptoListArray.filter(createFilter(this.state.inputValue, KEYS_TO_FILTERS))
-
-    if (this.state.cryptoListArray.length < 1 || this.state.coinSelect.length < 1) {
-      return (
-        <header class='contain-input-search'>
-          <div class="input-group mb-2 mr-sm-2">
-            <div class="input-group-prepend">
-              <div class="input-group-text">@</div>
-            </div>
-
-            {
-              this.state.cryptoListArray.length < 1 ?
-                <SearchInput type="text" disabled className="search-input form-control" onChange={this.keepInput} id="inlineFormInputGroupUsername2" placeholder="Search Coin" />
-                :
-                <SearchInput type="text" className="search-input form-control" onChange={this.keepInput} id="inlineFormInputGroupUsername2" placeholder="Search Coin" />
-            }
-          </div>
-          <div class='contain-select'>
-            {
-              this.state.inputValue ?
-                filterCoins.map(coin => {
-                  return (
-                    <div className="coins" onClick={() => this.onClickCoin(coin.id)} key={coin.id}>
-                      <img src={coin.img} />
-                      <h1 className="subject">{coin.id}</h1>
-                    </div>
-                  )
-                })
-                :
-                ''
-            }
-          </div>
-        </header>
-      )
-    }
-
     return (
       <header class='contain-input-search'>
-        <div class="input-group mb-2 mr-sm-2">
-          <div class="input-group-prepend">
-            <div class="input-group-text">@</div>
-          </div>
-          <SearchInput type="text" value={this.state.inputValue} className="search-input form-control" onChange={this.keepInput} id="inlineFormInputGroupUsername2" placeholder="Search Coin" />
-        </div>
-        <div class='contain-select'>
-          {
-            this.state.inputValue ?
-              filterCoins.map(coin => {
-                return (
-                  <div className="coins" onClick={() => this.onClickCoin(coin.id)} key={coin.id}>
-                    <img src={coin.img} />
-                    <h1 className="subject">{coin.id}</h1>
-                  </div>
-                )
-              })
-              :
-              ''
-          }
-        </div>
-        <section className='app-content'>
-          <table>
-            <tbody>
-              <CSSTransitionGroup
-                transitionName="example"
-                transitionEnterTimeout={500}
-                transitionLeaveTimeout={300}>
-                {this.state.coinSelect.map(selected => {
-                  return this.state.cryptoListArray.map(coin => {
-                    if (selected === coin.id) return <RowComponent dataCoin={coin} />
-                  })
-                })}
-              </CSSTransitionGroup>
-            </tbody>
-          </table>
-        </section>
-
+        <InputSearch inputValue={this.state.inputValue} cryptoListArray={this.state.cryptoListArray} keepInput={this.keepInput} />
+        <SelectInput inputValue={this.state.inputValue} onClickCoin={this.onClickCoin} filterCoins={filterCoins} />
+        {this.state.cryptoListArray.length < 1 ? '' : this.state.coinSelect.length < 1 ? '' : <ContentRowSelect coinSelect={this.state.coinSelect} cryptoListArray={this.state.cryptoListArray} />}
       </header>
-
     )
   }
 }
---
+++ src/components/ContentApp/Search/components/InputSearch.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import SearchInput from 'react-search-input'
+export default function InputSearch(props) {
+    
+    return (
+        <div class="input-group mb-2 mr-sm-2">
+            <div class="input-group-prepend">
+              <div class="input-group-text">@</div>
+            </div>
+
+            {
+              props.cryptoListArray.length < 1 ?
+                <SearchInput type="text" disabled className="search-input form-control" value ={props.inputValue} id="inlineFormInputGroupUsername2" placeholder="Search Coin" />
+                :
+                <SearchInput type="text" className="search-input form-control" value ={props.inputValue} onChange={props.keepInput} id="inlineFormInputGroupUsername2" placeholder="Search Coin" />
+            }
+          </div>
+    )
+}
